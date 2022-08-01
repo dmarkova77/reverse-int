1f69ac4eeaa71c0d046a86fa0c8cce87a796a4bf
Diana.Markova <dianamarkova518@gmail.com>
2022-08-01 11:56:21 +0300

    finished task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    console.log(n);
+    let array = n.toString().split('');
+    console.log(array);
+
+    if (array[0] == '-') {
+        let result = array.reverse().join('');
+        return parseFloat(result);
+    }
+   
+    return array.reverse().join('');
 }
