f89700060515bf9fb2badb43b75eaa793d888211
Diana <102411070+dmarkova77@users.noreply.github.com>
2023-01-17 16:21:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
 module.exports = function reverse (n) {
-    console.log(n);
-    let array = n.toString().split('');
-    console.log(array);
-
-    if (array[0] == '-') {
-        let result = array.reverse().join('');
-        return parseFloat(result);
-    }
-   
-    return array.reverse().join('');
+    return Math.abs(n).toString().split('').reverse().join('');
 }
